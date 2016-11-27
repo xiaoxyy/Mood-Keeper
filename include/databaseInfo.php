61f59b8d291a2be8303d98cac6497709b3674348
Xiao Yang Yu <xyyxiaoyangyu@gmail.com>
2016-11-27 15:16:46 -0500

    Update databaseInfo.php

--- include/databaseInfo.php
+++ include/databaseInfo.php
@@ -1,8 +1,7 @@
 <!-- Private information for access to database. -->
 
 <?php
-    $ip = "79.170.40.246"; // Using IP instead of localhost for other people to deploy.
-    define("ADDRESS", $ip);
+    define("ADDRESS", "localhost");
     define("USERNAME", "cl23-userinfo");
     define("PASSWORD", "abiltoableto");
     define("DBNAME", "cl23-userinfo");
